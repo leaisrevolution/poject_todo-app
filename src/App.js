bd8174c1e4444ffba240f295fb0d685305e645cc
leaisrevolution <leaisrevolution@gmail.com>
2021-12-04 01:44:43 +0900

    += = 에러 해결

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
 
   const onRemove = useCallback(
     id => {
-      setTodos(todos.filter(todo => todo.id != = id));
+      setTodos(todos.filter(todo => todo.id != id));
     },
     [todos],
   );
